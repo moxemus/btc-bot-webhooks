a30b2e0b12e7ae04b09e7410777882c40d953ab1
Maksim Partsiankou <maksim.part@gmail.com>
2023-01-10 22:12:38 +0300

    add autoload

--- public/index.php
+++ public/index.php
@@ -1,20 +1,6 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../src/config/DB.php';
-require __DIR__ . '/../src/config/Logger.php';
-
-
-require __DIR__ . '/../src/components/rateApi/BaseAdaptor.php';
-require __DIR__ . '/../src/components/rateApi/BlockchainAdaptor.php';
-
-require __DIR__ . '/../src/components/telegram/Adaptor.php';
-require __DIR__ . '/../src/components/telegram/Handler.php';
-require __DIR__ . '/../src/components/telegram/Response.php';
-
-require __DIR__ . '/../src/components/schedule/Handler.php';
-
-
 
 // Instantiate App
 use Slim\Factory\AppFactory;
